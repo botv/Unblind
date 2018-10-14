b6459347d3bae2d7743075eb67fba7f98a105f9e
Ben Botvinick <benbotvinick@gmail.com>
2018-10-14 04:54:27 -0400

    easy fix

--- functions/summarize_text.py
+++ functions/summarize_text.py
@@ -12,8 +12,8 @@ def sort_string(blocks):
 		char_area = text_["height"] * text_["width"] / len(text_["text"])
 		output.update({text: char_area})
 	sorted_output = sorted(output, key=output.get, reverse=True)
-	count = 0
-	while count < 3 | count < len(sorted_output):
+	count = 1
+	while count < 4 | count < len(sorted_output):
 		top_3.append(sorted_output[count])
 		count = count + 1
 	return top_3
